0bc039f6f70d53865284245ea3c2da2864c40a71
Mladen Todorovic <mladen.todorovic@burda.com>
2017-01-25 18:02:49 +0100

    Remove not needed class

--- js/paragraphs.modal.js
+++ js/paragraphs.modal.js
@@ -7,11 +7,6 @@
 
   'use strict';
 
-  Drupal.AjaxCommands.prototype.paragraphs_add_paragraph = function (ajax, response, status) {
-    $('select[data-uuid="' + response.uuid + '"]').val(response.paragraph_type);
-    $('input[data-uuid="' + response.uuid + '"]').trigger('mousedown');
-  };
-
   Drupal.behaviors.bodyTabs = {
     attach: function (context, settings) {
       $('.paragraphs_types_container ul', context).each(function () {
